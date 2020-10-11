cdead0f7be4966df86f95a4c7a05eb797079ab6e
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-10-11 21:43:39 +0700

    Add Swagger UI

---
+++ src/main/java/ru/yegorr/todolist/swagger/SpringFoxConfig.java
@@ -0,0 +1,44 @@
+package ru.yegorr.todolist.swagger;
+
+import org.springframework.context.annotation.*;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * Config for Swagger
+ */
+@Configuration
+public class SpringFoxConfig {
+
+    private static ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Todolist API",
+                "Description of Todolist API",
+                "0.0.1",
+                "Terms of service",
+                new Contact("Egor Ryazantsev", "https://github.com/YegorR", "egorryazancev1908@mail.ru"),
+                "Licence",
+                "https://blank.org/",
+                Collections.emptyList()
+        );
+    }
+
+    /**
+     * Configures Swagger
+     *
+     * @return Docket
+     */
+    @Bean
+    public static Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
