7531f3d3ab6fba401c9509299bab3147db3a3e33
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-10-12 18:25:45 +0700

    Fix exception system

--- src/main/java/ru/yegorr/todolist/controller/errorhandling/ErrorHandler.java
+++ src/main/java/ru/yegorr/todolist/controller/errorhandling/ErrorHandler.java
@@ -5,8 +5,8 @@ import org.springframework.http.converter.*;
 import org.springframework.web.*;
 import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import ru.yegorr.todolist.dto.response.ExceptionResponse;
 import ru.yegorr.todolist.exception.NotFoundException;
 
@@ -14,67 +14,64 @@ import ru.yegorr.todolist.exception.NotFoundException;
  * Handler of application exceptions
  */
 @ControllerAdvice
-public class ErrorHandler extends ResponseEntityExceptionHandler {
+public class ErrorHandler {
 
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    private static ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
         return generateDefaultExceptionResponse(String.format("%s is not supported method", ex.getMethod()), HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
-            HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({HttpMediaTypeNotSupportedException.class})
+    private static ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex) {
         return generateDefaultExceptionResponse(String.format("%s is not supported media type", ex.getContentType()), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
-            HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({HttpMediaTypeNotAcceptableException.class})
+    private static ResponseEntity<Object> handleHttpMediaTypeNotAcceptable() {
         return generateDefaultExceptionResponse("Not acceptable", HttpStatus.NOT_ACCEPTABLE);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMissingPathVariable(
-            MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({MissingPathVariableException.class})
+    private static ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex) {
         return generateDefaultExceptionResponse(String.format("%s path variable is needed", ex.getVariableName()), HttpStatus.BAD_REQUEST);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMissingServletRequestParameter(
-            MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    private static ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
         return generateDefaultExceptionResponse(
                 String.format("%s(%s) parameter is missing", ex.getParameterName(), ex.getParameterType()), HttpStatus.BAD_REQUEST);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(
-            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    private static ResponseEntity<Object> handleHttpMessageNotReadable() {
         return generateDefaultExceptionResponse("Server can not read the http message", HttpStatus.BAD_REQUEST);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request
-    ) {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    private static ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         return generateDefaultExceptionResponse(String.format("%s(%s) parameter validation fails", ex.getParameter().getParameterName(),
-                ex.getParameter().getParameterType().getSimpleName()), HttpStatus.BAD_REQUEST);
+                ex.getParameter().getParameterType().getSimpleName()
+        ), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({NotFoundException.class})
-    protected ResponseEntity<Object> handleNotFound(NotFoundException ex) {
+    private static ResponseEntity<Object> handleNotFound(NotFoundException ex) {
         return generateDefaultExceptionResponse(String.format("%s is not found", ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({Throwable.class})
-    protected ResponseEntity<Object> handleOther() {
+    private static ResponseEntity<Object> handleOther() {
         return generateDefaultExceptionResponse("Some server error has happened", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+    private static ResponseEntity<Object> handleMethodArgumentTypeMismatchException() {
+        return generateDefaultExceptionResponse("Argument type are wrong", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    private static ResponseEntity<Object> handleNoHandlerFound() {
+        return generateDefaultExceptionResponse("The page is not found", HttpStatus.NOT_FOUND);
     }
 
     private static ResponseEntity<Object> generateDefaultExceptionResponse(String message, HttpStatus httpStatus) {
