197c9591b9dc929780b2c0e5610b0b8ff1317931
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-10-18 17:00:21 +0700

    Update action

--- src/main/java/ru/yegorr/todolist/service/filtering/Action.java
+++ src/main/java/ru/yegorr/todolist/service/filtering/Action.java
@@ -2,14 +2,24 @@ package ru.yegorr.todolist.service.filtering;
 
 import lombok.Value;
 
+import java.util.List;
+
 /**
- * @param <T> Action for filtering
+ * Action for filtering.
+ * If ActionType is AND, OR, NOT, then actions must not be null or empty, else property and value must not be null or empty
+ *
  */
 @Value
-public class Action<T> {
-    String key;
+public class Action {
+    public enum ActionType {
+        AND, OR, NOT, EQUAL, MORE, LESS, MORE_OR_EQUAL, LESS_OR_EQUAL, NOT_EQUAL
+    }
 
-    T value;
+    ActionType actionType;
 
-    String operator;
+    List<Action> actions;
+
+    String property;
+
+    Object value;
 }
