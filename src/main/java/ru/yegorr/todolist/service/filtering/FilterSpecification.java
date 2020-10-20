4aa0a48aab6259afb304575cb81a956ce9b3db3a
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-10-20 13:27:47 +0700

    Fix FilterSpecification
    Do casting to necessary type

--- src/main/java/ru/yegorr/todolist/service/filtering/FilterSpecification.java
+++ src/main/java/ru/yegorr/todolist/service/filtering/FilterSpecification.java
@@ -1,8 +1,10 @@
 package ru.yegorr.todolist.service.filtering;
 
 import org.springframework.data.jpa.domain.Specification;
+import ru.yegorr.todolist.exception.ValidationFailsException;
 
 import javax.persistence.criteria.*;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -41,11 +43,31 @@ public class FilterSpecification<T> implements Specification<T> {
             case OR -> criteriaBuilder.or(getPredicatesFromActions(action.getActions(), root, query, criteriaBuilder));
             case NOT -> criteriaBuilder.not(new FilterSpecification<T>(action.getActions().get(0)).toPredicate(root, query, criteriaBuilder));
             case EQUAL -> criteriaBuilder.equal(root.get(action.getProperty()), action.getValue());
-            case MORE -> criteriaBuilder.greaterThan(root.get(action.getProperty()), action.getValue().toString());
-            case LESS -> criteriaBuilder.lessThan(root.get(action.getProperty()), action.getValue().toString());
-            case MORE_OR_EQUAL -> criteriaBuilder.greaterThanOrEqualTo(root.get(action.getProperty()), action.getValue().toString());
-            case LESS_OR_EQUAL -> criteriaBuilder.lessThanOrEqualTo(root.get(action.getProperty()), action.getValue().toString());
             case NOT_EQUAL -> criteriaBuilder.notEqual(root.get(action.getProperty()), action.getValue());
+            case MORE, MORE_OR_EQUAL, LESS, LESS_OR_EQUAL -> {
+                Object value = action.getValue();
+                if (value instanceof LocalDate) {
+                    yield moreOrLessPredicate(root, criteriaBuilder, (LocalDate)value);
+                } else if (value instanceof Integer) {
+                    yield moreOrLessPredicate(root, criteriaBuilder, (Integer)value);
+                } else if (value instanceof Boolean) {
+                    yield moreOrLessPredicate(root, criteriaBuilder, (Boolean)value);
+                } else if (value instanceof String) {
+                    yield moreOrLessPredicate(root, criteriaBuilder, (String)value);
+                } else {
+                    throw new IllegalStateException("Unexpected value type in filter");
+                }
+            }
+        };
+    }
+
+    private <Y extends Comparable<? super Y>> Predicate moreOrLessPredicate(Root<T> root, CriteriaBuilder criteriaBuilder, Y object) {
+        return switch (action.getActionType()) {
+            case MORE -> criteriaBuilder.greaterThan(root.get(action.getProperty()), object);
+            case LESS -> criteriaBuilder.lessThan(root.get(action.getProperty()), object);
+            case MORE_OR_EQUAL -> criteriaBuilder.greaterThanOrEqualTo(root.get(action.getProperty()), object);
+            case LESS_OR_EQUAL -> criteriaBuilder.lessThanOrEqualTo(root.get(action.getProperty()), object);
+            default -> throw new IllegalStateException("Not exception state: " + action.getActionType());
         };
     }
 }
