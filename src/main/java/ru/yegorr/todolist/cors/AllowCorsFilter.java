fd464e93d24afa6411cc63f3c0c59fc158d3ae46
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-10-28 10:34:39 +0700

    Добавил Access-Control-Allow-Headers в CORS

--- src/main/java/ru/yegorr/todolist/cors/AllowCorsFilter.java
+++ src/main/java/ru/yegorr/todolist/cors/AllowCorsFilter.java
@@ -19,7 +19,7 @@ public class AllowCorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-
+        response.setHeader("Access-Control-Allow-Headers", "*");
         filterChain.doFilter(request, response);
     }
 }
