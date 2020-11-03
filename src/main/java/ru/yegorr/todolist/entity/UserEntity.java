876f0eb7220e8e55d8a58f8ab52c444f00e4bed6
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-03 13:27:10 +0700

    Сделал roles для UserEntity раннюю загрузку

--- src/main/java/ru/yegorr/todolist/entity/UserEntity.java
+++ src/main/java/ru/yegorr/todolist/entity/UserEntity.java
@@ -36,11 +36,11 @@ public class UserEntity {
     @Column(name = "refresh_exp")
     private LocalDateTime refreshExp;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")}
     )
-    private Set<RoleEntity> roles;
+    private List<RoleEntity> roles;
 }
