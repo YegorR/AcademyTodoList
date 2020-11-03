626da5319b6dc318c615ad889effda339aaf2a79
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-03 13:27:41 +0700

    Сделал в Swagger поодержку токенов

--- src/main/java/ru/yegorr/todolist/swagger/SpringFoxConfig.java
+++ src/main/java/ru/yegorr/todolist/swagger/SpringFoxConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.*;
 import springfox.documentation.builders.*;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.*;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.nio.file.Path;
-import java.util.Collections;
-import java.util.function.Predicate;
+import java.util.*;
 
 /**
  * Конфигурация списка
  */
 @Configuration
 public class SpringFoxConfig {
+
+    private static final String KEY_NAME = "JWT";
 
     private static ApiInfo apiInfo() {
         return new ApiInfo(
@@ -41,6 +42,20 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any().and(PathSelectors.regex("/error").negate()))
-                .build().useDefaultResponseMessages(false);
+                .build().useDefaultResponseMessages(false).securitySchemes(List.of(apiKey())).securityContexts(List.of(securityContext()));
+    }
+
+    private static ApiKey apiKey() {
+        return new ApiKey(KEY_NAME, "Authorization", "header");
+    }
+
+    private static SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).operationSelector(operationContext -> true).build();
+    }
+
+    private static List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = {authorizationScope};
+        return List.of(new SecurityReference(KEY_NAME, authorizationScopes));
     }
 }
