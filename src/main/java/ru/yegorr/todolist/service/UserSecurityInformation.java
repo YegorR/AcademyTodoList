5161bc037859bec39cfcbd6aa3d0599ead6e21e1
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-04 10:23:12 +0700

    Добавил вспомогательный метод isAdmin

--- src/main/java/ru/yegorr/todolist/service/UserSecurityInformation.java
+++ src/main/java/ru/yegorr/todolist/service/UserSecurityInformation.java
@@ -1,13 +1,19 @@
 package ru.yegorr.todolist.service;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import ru.yegorr.todolist.entity.Role;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Вспомогательный класс для быстрого получения id пользователя, работающего в системе
  */
 public class UserSecurityInformation {
+
+    private UserSecurityInformation() {
+    }
 
     /**
      * Получает id пользователя, работающего в системе
@@ -16,5 +22,21 @@ public class UserSecurityInformation {
      */
     public static UUID getUserId() {
         return (UUID)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    /**
+     * Проверяет, является ли пользователь админом
+     *
+     * @return true, если админ, иначе false
+     */
+    public static boolean isAdmin() {
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList())
+                .contains(Role.ROLE_ADMIN.name());
     }
 }
