f97a071e25515a8e04d02a11461b8ba6c2c92d06
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-04 12:01:29 +0700

    Обновил документацию и валидацию для AuthenticateRequest

--- src/main/java/ru/yegorr/todolist/dto/request/AuthenticateRequest.java
+++ src/main/java/ru/yegorr/todolist/dto/request/AuthenticateRequest.java
@@ -1,13 +1,19 @@
 package ru.yegorr.todolist.dto.request;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
+
+import javax.validation.constraints.*;
 
 /**
  * Запрос для аутентификации
  */
 @Data
+@ApiModel("Запрос для аутентификации")
 public class AuthenticateRequest {
+    @Email(message = "{email.wrong}")
     private String email;
 
+    @NotBlank(message = "{password.notblank}")
     private String password;
 }
