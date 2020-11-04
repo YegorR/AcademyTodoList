b74e7343d4ed57a40b0470bb10bf47c30506dfd8
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-04 10:45:16 +0700

    Добавил вспомогательный метод checkAdminOrThisUser

--- src/main/java/ru/yegorr/todolist/service/UserSecurityInformation.java
+++ src/main/java/ru/yegorr/todolist/service/UserSecurityInformation.java
@@ -3,6 +3,7 @@ package ru.yegorr.todolist.service;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import ru.yegorr.todolist.entity.Role;
+import ru.yegorr.todolist.exception.ForbiddenException;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -38,5 +39,11 @@ public class UserSecurityInformation {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList())
                 .contains(Role.ROLE_ADMIN.name());
+    }
+
+    public static void checkAdminOrThisUser(UUID userId) throws ForbiddenException {
+        if (!(isAdmin() || getUserId().equals(userId))) {
+            throw new ForbiddenException();
+        }
     }
 }
