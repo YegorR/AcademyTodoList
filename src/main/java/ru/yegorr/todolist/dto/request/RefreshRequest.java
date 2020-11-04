0f7c209e719a44901e26218cf5d95aa912641961
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-04 12:04:41 +0700

    Обновил документацию и валидацию для RefreshRequest

--- src/main/java/ru/yegorr/todolist/dto/request/RefreshRequest.java
+++ src/main/java/ru/yegorr/todolist/dto/request/RefreshRequest.java
@@ -1,11 +1,16 @@
 package ru.yegorr.todolist.dto.request;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * Запрос для обновления токенов
  */
 @Data
+@ApiModel("Запрос для обновления токенов")
 public class RefreshRequest {
+    @NotBlank(message = "{refreshToken.notBlank}")
     private String refreshToken;
 }
