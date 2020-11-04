380d7fad325b4f8539fa0e0b0d6eb27ba0106f38
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-11-04 13:41:04 +0700

    Создал Encoder

---
+++ src/main/java/ru/yegorr/todolist/security/Encoder.java
@@ -0,0 +1,30 @@
+package ru.yegorr.todolist.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Делает хэши паролей
+ */
+@Component
+public class Encoder {
+    private final PasswordEncoder encoder;
+
+    /**
+     * Конструктор
+     */
+    public Encoder() {
+        encoder = new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Создаёт строку-хэш пароля
+     *
+     * @param password пароль
+     * @return хэш
+     */
+    public String encryptPassword(String password) {
+        return encoder.encode(password);
+    }
+}
